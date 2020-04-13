2c1437d9c6607823725f4407ae284e494cec903b
DevanTarunKumar <37919626+DevanTarunKumar@users.noreply.github.com>
2020-04-13 14:08:02 -0500

    upgrading composer (#8)

--- bin/init/install_composer.sh
+++ bin/init/install_composer.sh
@@ -4,19 +4,19 @@ set -eu
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
-EXPECTED_VERSION=1.9.0
-EXPECTED_CHECKSUM=c9dff69d092bdec14dee64df6677e7430163509798895fbd54891c166c5c0875
+EXPECTED_VERSION=1.10.1
+EXPECTED_CHECKSUM=b6112e167c56731fe2c6b210f7a6c07da544ffe33d31d0e9676c411c59ffffbf55604f19b3ea49b6c43d2f6c54db52a2
 
 # Check for existing installation
-if [ -e "${ROOT}"/bin/composer ]; then
+if [ -e "$ROOT"/bin/composer ]; then
     echo "Checking Composer version..."
-    FOUND_VERSION=`( \
-        cd "${ROOT}" && \
+    FOUND_VERSION=$( \
+        cd "$ROOT" && \
         ./bin/composer --version | \
         tail -1 | \
         awk '{print $3}' | \
-        "${ROOT}"/bin/linux-sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" \
-    )`
+        "$ROOT"/bin/linux-sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" \
+    )
 
     # Check version of installed composer
     if [ "$FOUND_VERSION" == "$EXPECTED_VERSION" ]; then
@@ -26,26 +26,26 @@ if [ -e "${ROOT}"/bin/composer ]; then
     else
         # Version does not match. Remove it.
         echo "Removing Composer version ${FOUND_VERSION}..."
-        rm "${ROOT}"/bin/composer
+        rm "$ROOT"/bin/composer
     fi
 fi
 
 # Install composer
 echo "Installing Composer ${EXPECTED_VERSION}..."
-curl -L https://getcomposer.org/download/${EXPECTED_VERSION}/composer.phar > "${ROOT}"/bin/composer
+curl -L https://getcomposer.org/download/${EXPECTED_VERSION}/composer.phar > "$ROOT"/bin/composer
 
 echo -n "Checking the hash..."
-
-if [[ ! $(shasum -a 256 "${ROOT}"/bin/composer) = "$EXPECTED_CHECKSUM"* ]]; then
-    echo "ERROR: composer's sha256 doesn't match. Redownload it."
-    exit 0
+if [[ ! $(shasum -a 384 "${ROOT}"/bin/composer) = "$EXPECTED_CHECKSUM"* ]]; then
+    echo "ERROR: composer's sha384 doesn't match. Redownload it."
+    rm "${ROOT}"/bin/composer
+    exit 1
 else
     echo "passed"
 fi
 
 # Fix permissions
 echo "Setting composer permissions..."
-chmod 755 "${ROOT}"/bin/composer
+chmod 755 "$ROOT"/bin/composer
 
 # Ensure ~/.composer exists for the Docker container to mount
 echo "Initializing ~/.composer directory..."
